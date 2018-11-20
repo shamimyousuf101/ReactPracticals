f2798b3534c9d0dee67c2f139f5f0fa088f03e1e
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2018-11-20 15:47:59 +0000

    add default search result

--- src/DynamicContent/Search.js
+++ src/DynamicContent/Search.js
@@ -1,33 +1,87 @@
-import React from 'react';
 
-const Search = (props) => {
+import React, { Component } from 'react';
 
-    const promotionData = [
-        {"name":"lapse Prevention", "startDate": "01 Jan 2019", "endDate": "20 Jan 2019"},
-        {"name":"Welcome", "startDate": "03 Jan 2019", "endDate": "20 Feb 2019"},
-        {"name":"Welcome Back", "startDate": "06 Jan 2019", "endDate": "22 Jan 2019"},
-        {"name":"Cash Back", "startDate": "18 Jan 2019", "endDate": "30 Jan 2019"},
-        {"name":"Play Day", "startDate": "01 Jan 2019", "endDate": "01 Mar 2019"}
-    ]
 
-    const searchBtnClick = (event) => {
-        const searchInput = document.querySelector(".searchInput");
-        event.preventDefault();  
-        console.log("search data is   " + searchInput.value);
+class Search extends Component {
+
+    state = {
+        searchTerm:"",
+        searchDisplay:"no matches found"
+    };
+
+    promotionData = {
+        "promotions" : [
+            {"name":"lapse Prevention", "startDate": "01 Jan 2019", "endDate": "20 Jan 2019"},
+            {"name":"Welcome", "startDate": "03 Jan 2019", "endDate": "20 Feb 2019"},
+            {"name":"Welcome Back", "startDate": "06 Jan 2019", "endDate": "22 Jan 2019"},
+            {"name":"Cash Back", "startDate": "18 Jan 2019", "endDate": "30 Jan 2019"},
+            {"name":"Play Day", "startDate": "01 Jan 2019", "endDate": "01 Mar 2019"}
+        ]
     }
 
-    return(
-        <div className="searchBox">
+    handleInputChange=(event) => {
+        const searchTerm = event.target.value;
+        this.setState({searchTerm});
 
-        <form className="searchForm">
-            <input className="searchInput" type="text" placeholder="Search"/>
-            <button className="searchbtn" type="submit" onClick={searchBtnClick}> 
-              <img src="https://www.mockplus.com/images/search.png"/>
-            </button>                    
-          </form>
-                            
-        </div>
-    )
+    }
+
+    searchBtnClick = (event) => {
+        let  resultArr = [];
+        
+        const searchInput = event.target;
+        event.preventDefault();  
+        console.log("search data is   " + searchInput.value);
+
+        var arr = [];
+
+
+        for(let i in this.promotionData.promotions){
+            arr.push(this.promotionData.promotions[i].name);
+        }
+
+        function filterItems(query) {
+            return arr.filter(function(el) {
+                return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+            })
+        }
+
+        resultArr = filterItems(searchInput.value);
+
+        console.log("resultArr.length   is equal to " + resultArr.length);
+
+        if(resultArr.length === 0){
+            this.setState({searchDisplay: 'No Matches Found'});
+           
+        }else{
+            this.setState({searchDisplay: 'Found'});
+        }
+    }
+
+    render(){
+
+
+        return(
+            <div className="searchBox">
+    
+            <form className="searchForm">
+                <input className="searchInput" type="text" placeholder="Search" value={this.state.searchTerm} onChange={this.handleInputChange}/>
+                <button className="searchbtn" type="submit" onClick={this.searchBtnClick}> 
+                  <img src="https://www.mockplus.com/images/search.png"/>
+                </button>                    
+              </form>
+    
+              <div className="searchResult">             
+
+                <div>
+                {this.state.searchDisplay}
+                </div>
+              
+              </div>
+                                
+            </div>
+        );
+    }
+    
 
 }
 
