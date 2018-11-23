a44d44f88344919064d14d23acb148b533060153
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2018-11-23 18:00:35 +0000

    review comments on css ex[ect css variables

--- src/Components/Header/Banner.js
+++ src/Components/Header/Banner.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-const Banner = ({subHeading}) =>  (<div className="banner"><h1>Promotion Manager</h1><h3>{subHeading}</h3></div>);
+const Banner = ({subHeading}) =>  (<header className="banner"><h1>Promotion Manager</h1><h3>{subHeading}</h3></header>);
 export default Banner;
