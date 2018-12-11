f6f28904dc7cf1ed9905b23ff7ffd50518b3c0bb
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2018-12-11 18:08:24 +0000

    promotion images

--- src/Components/DynamicContent/Search/PromotionImage.js
+++ src/Components/DynamicContent/Search/PromotionImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PromotionImage = (props) => {
-    const imgStyle = {width: '500px', height: '600px'} ;
+    const imgStyle = {width: '100px', height: '100px'} ;
     return(          
         <img src={props.result.asset} alt="promotion" style={imgStyle} />
     )
