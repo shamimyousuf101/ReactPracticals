85609e578f68b1e9b35b81850d12e30ccc6c6559
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2018-12-28 12:07:34 +0000

    clean up

--- src/Components/DynamicContent/Container.js
+++ src/Components/DynamicContent/Container.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import AssetManager from './../../Components/DynamicContent/Asset/AssetManager';
 import Search from './../../Components/DynamicContent/Search/Search';
--- src/Components/DynamicContent/PromotionBuilder/promotionData.js
+++ src/Components/DynamicContent/PromotionBuilder/promotionData.js
@@ -1,42 +1,37 @@
 export const promotionData = {
 
 
+    "1212" : {
 
-  
-                "1212" : {
-
-                             "id": "1212",
-                             "name":"welcome", 
-                            "url":"welcome url", 
-                            "devices": ["mobile", "tablet"], 
-                            "ventures": ["jackpotjoy", "starspins"]
-                        },
-   
-
-
-                "343434" : {
-
-                             "id": "343434",
-                             "name":"welcome back", 
-                            "url":"welcome back url", 
-                            "devices": ["mobile", "tablet", "desktop"], 
-                            "ventures": ["jackpotjoy", "botemania"]
-                        },
-
-
-                "422" : {
-
-                    "id": "422",
-                    "name":"lapse", 
-                   "url":"lapse url", 
-                   "devices": ["mobile"], 
-                   "ventures": [ "starspins", "heartbingo"]
-               }
-   
+        "id": "1212",
+        "name":"welcome", 
+        "url":"welcome url", 
+        "devices": ["mobile", "tablet"], 
+        "ventures": ["jackpotjoy", "starspins"]
+    },
 
 
 
-        
-    };
+    "343434" : {
+
+        "id": "343434",
+        "name":"welcome back", 
+        "url":"welcome back url", 
+        "devices": ["mobile", "tablet", "desktop"], 
+        "ventures": ["jackpotjoy", "botemania"]
+    },
+
+
+    "422" : {
+
+        "id": "422",
+        "name":"lapse", 
+        "url":"lapse url", 
+        "devices": ["mobile"], 
+        "ventures": [ "starspins", "heartbingo"]
+    }
+
+
+};
 
 
