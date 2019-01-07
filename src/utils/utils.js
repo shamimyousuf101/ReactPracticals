14ec02e024d71a857f0bd66d44074d5442948a4b
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-01-07 19:05:38 +0000

    incorporated review comments

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,10 @@
 
 
         export const getTruthyList = (list) => {
-
+          
             let listArray = Array.from(list.entries());
             let truthyList = listArray.filter( item => item[1] === true )
-            let str = "";
-
-            truthyList.forEach(
-            item => {
-                str = str + item[0] + ", "}
-            )
-
-            return str;
-
+            return truthyList.join(',');
         }
 
 
