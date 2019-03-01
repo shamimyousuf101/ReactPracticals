ffabc0d818d8fbe769f7c0bf323f4ae105605bcc
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-03-01 17:53:56 +0000

    search changes

--- src/promotion-builder/features/search.js
+++ src/promotion-builder/features/search.js
@@ -1,15 +1,15 @@
 
 export const searchPromotions = (searchInput, promotionData) => {
-  const promotionArray = mapToArray(promotionData);
+  const promotionArray = Object.keys(promotionData).map(key => promotionData[key]);
   const filteredPromotionArray = search(searchInput, promotionArray);
-  return sort(filteredPromotionArray);
+  return filteredPromotionArray.sort((a, b) => b.lastUpdatedTime - a.lastUpdatedTime);
 };
 
 
 export const searchPromotionsById = (searchInput, promotionData) => {
-  const promotionArray = mapToArray(promotionData);
+  const promotionArray = Object.keys(promotionData).map(key => promotionData[key]);
   const filteredPromotionArray = searchById(searchInput, promotionArray);
-  return sort(filteredPromotionArray);
+  return filteredPromotionArray.sort((a, b) => b.lastUpdatedTime - a.lastUpdatedTime);
 };
 
 export const search = (searchInput, itemArray = []) => {
@@ -22,12 +22,4 @@ export const searchById = (searchInput, itemArray = []) => {
   return itemArray.filter(
     item => item.id.toLowerCase().indexOf(searchInput.toLowerCase()) > -1
   );
-};
-
-export const sort = itemArray => {
-  return itemArray.sort((a, b) => b.lastUpdatedTime - a.lastUpdatedTime);
-};
-
-export const mapToArray = itemMap => {
-  return Object.keys(itemMap).map(key => itemMap[key]);
 };
