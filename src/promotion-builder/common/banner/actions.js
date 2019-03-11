e7a6c0c0f7b69e09baf2fb6f569c84bdea7c9802
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-03-11 17:18:59 +0000

    added actions and reducer for banner

---
+++ src/promotion-builder/common/banner/actions.js
@@ -0,0 +1,20 @@
+/*
+* action types
+*/
+
+export const ADD_SUBHEADING = 'ADD_SUBHEADING'
+
+/*
+* other constants
+*/
+
+/*
+* action creators
+*/
+
+export const addSubheading = (text) => {
+    return {
+        type: ADD_SUBHEADING,
+        payload: text
+    }
+}
---
+++ src/promotion-builder/common/banner/reducer.js
@@ -0,0 +1,16 @@
+import { ADD_SUBHEADING } from "./actions";
+
+import { VIEW } from "../../../constants/constants";
+
+const initialState = {
+  view: VIEW.SEARCH
+};
+
+const getSubHeading = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SUBHEADING:
+      return Object.assign({}, state, { view: action.payload });
+    default:
+      return state;
+  }
+};
